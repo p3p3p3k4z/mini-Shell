f5315ee75fa70174f8123954f6b6917f26dff04f
Mario Ramirez <ragm030119@gs.utm.mx>
2025-05-05 13:15:14 -0600

    corregido

--- miniS.c
+++ miniS.c
@@ -1,3 +1,12 @@
+/*
+ PROGRAMA: Mini-Shell
+ INTEGRANTES: Espina Ramirez Ariadna Betsabe
+							Ramirez Gallardo Mario Enrique
+							Perez Zurita Irving Tristan
+ COMPILACION: gcc -Wall -o "miniS" "miniS.c"
+ FECHA: 05-05-2025
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,37 +26,46 @@ int main() {
     char *argv[MAX_PROCESOS][MAX_ARG]; //Un puntero doble para guardar los argumentos de cada comando
     int tubes[MAX_PROCESOS - 1][2]; // Matriz de tuberías para comunicar procesos
     pid_t pid;
-    int n_comandos; // Número de comandos separados por '|'
+    int n_comandos; // Número de comandos separados
 
-    printf("Hola este es un MiniShell.\n");
+    printf("\t\t==Bienvenido este es un MiniShell.==\n\n");
 
     while (1) {
-        printf("> ");
+        printf(" > ");
+        //Leemos la linea
         if (fgets(linea, sizeof(linea), stdin) == NULL) {
             printf("\nSaliendo del MiniShell.\n");
             break;
         }
+        //quitar los saltos y cambiarlos por valor nulo
         linea[strcspn(linea, "\n")] = '\0';
 
         if (strlen(linea) == 0) { //Si el usuario no escribe nada, vuelve a pedir
             continue;
         }
 
-        n_comandos = dividir(linea, "|", comandos); //mochar 
-        
+		//dividimos cada linea por "|" y le enviamos el arrglo comandos
+        n_comandos = dividir(linea, "|", comandos); 
+
+        //Si no se detecto almenos un comando 
         if (n_comandos < 1) {
             fprintf(stderr, "Error: No hay comandos válidos.\n");
             continue;
         }
 
+		//Imprimir la primera tubería
         imprimir_tuberia(comandos, n_comandos);
-	 //Aquí se separa cada comando en palabras
+        
+		//Aquí se separa cada comando en palabras
         for (int i = 0; i < n_comandos; i++) {
             int argc = 0;
+            //dividimos la cadena en tokens con el delimitador, en este caso el delimitador " "
             char *arg = strtok(comandos[i], " ");
+            
             while (arg && argc < MAX_ARG - 1) {
                 argv[i][argc++] = arg;
-                arg = strtok(NULL, " ");
+                //Al final le agregamos un null
+                arg = strtok(NULL, "  ");
             }
             argv[i][argc] = NULL;
         }
@@ -62,42 +80,41 @@ int main() {
 
         // Ejecutar cada comando en un proceso hijo para cada comando
         for (int i = 0; i < n_comandos; i++) {
+			//Con fork creamos el proceso hijo
             pid = fork();
+            //Comprobamos que no exista algun error
             if (pid == -1) {
                 perror("Error al crear el proceso hijo");
                 return 1;
             }
 
-            if (pid == 0) { // Proceso hijo
+            if (pid == 0) { // Si ha funcionado seguimos:
+				
                 // Redirigir la entrada si no es el primer comando
                 if (i > 0) {
-                    dup2(tubes[i - 1][0], STDIN_FILENO);//recibe
+					//con dup2  el extremo escritur [0] se redirecciona a la entrada estandar
+                    dup2(tubes[i - 1][0], STDIN_FILENO);
                 }
 
-                // Redirigir la salida si no es el último comando
+                
                 if (i < n_comandos - 1) {
-                    dup2(tubes[i][1], STDOUT_FILENO);//sale
+					//Con la tuberia en su extremo de lectura [1] se redirecciona a la salida estandar
+                    dup2(tubes[i][1], STDOUT_FILENO);
                 }
 
-                // Cerrar los extremos INNECESARIOS de TODAS las tuberías en el hijo
+                // Cerrar los extremos  que no se ocuparan ya 
                 for (int j = 0; j < n_comandos - 1; j++) {
-                    if (i == 0) { // Primer comando: solo cierra los extremos de lectura
-                        close(tubes[j][0]);
-                    } else if (i == n_comandos - 1) { // Último comando: solo cierra los extremos de escritura
-                        close(tubes[j][1]);
-                    } else { // Comando intermedio: cierra el extremo de lectura de la tubería anterior y el extremo de escritura de la tubería siguiente
-                        if (j != i - 1) close(tubes[j][0]);
-                        if (j != i) close(tubes[j][1]);
-                    }
+                    close (tubes[j][0]);
+                    close (tubes[j][1]);
                 }
 
+               //Enviar el comando y el puntero donde termina cada uno 
                 execvp(argv[i][0], argv[i]);
                 perror("Error al ejecutar el comando");
-                exit(EXIT_FAILURE);
             }
         }
 
-        // El padre debe cerrar todos los descriptores de archivo de las tuberías
+        // El padre debe cerrar las tuberias
         for (int i = 0; i < n_comandos - 1; i++) {
             close(tubes[i][0]);
             close(tubes[i][1]);
@@ -112,18 +129,17 @@ int main() {
     return 0;
 }
 
-void error(const char *msg) {
-    perror(msg);
-    exit(EXIT_FAILURE);
-}
 
 int dividir(char *cadena, char *lim, char *tokens[]) {
     int conta = 0;
+    //cada "palabra" se guardara en una posicion del arreglo token
+    //el lim es el limitante que se ha enviado "|"
     char *token = strtok(cadena, lim);
     while (token != NULL && conta < MAX_PROCESOS) {
         tokens[conta++] = token; //Guarda el token en el arreglo tokens
         token = strtok(NULL, lim);//Se llama de nuevo a strtok, pero con NULL para continuar donde quedó
     }
+    //terminar el ultimo arreglo con null
     tokens[conta] = NULL;
     return conta;
 }
@@ -138,3 +154,4 @@ void imprimir_tuberia(char *tokens[], int num_tokens) {
     }
     printf("\n");
 }
+     
